6164805235e83b37fda29b860bb8dfa9d5023a4f
Emmanuel <emmanijoshy@live.com>
2021-03-13 07:58:41 +0530

    My first commit and push to Cloud Repo

---
+++ Python/f2.py
@@ -0,0 +1,5 @@
+By moving files with git, we notify git about two things
+
+The hello.html file was deleted.
+The lib/hello.html file was created.
+Both facts are staged immediately and ready for a commit. Git status command reports the file has been moved.:
